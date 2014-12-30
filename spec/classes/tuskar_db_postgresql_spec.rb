5727f1aae61eef22fc19fd6935488ab13f313254
Sebastien Badia <sebastien.badia@enovance.com>
2014-12-30 22:30:43 +0100

    Migrate postgresql backend to use openstacklib::db::postgresql
    Let migrate to the new openstacklib::db::postgresql helper for
    postgresql backend. This commit also unpin postgresql fixture
    (openstacklib support now the latest version of postgre module).
    
    Change-Id: Ibf9bf074f1fb1712c0c5d70faa7d29a3b72bce29
    Implements: blueprint commmon-openstack-database-resource

--- spec/classes/tuskar_db_postgresql_spec.rb
+++ spec/classes/tuskar_db_postgresql_spec.rb
@@ -3,24 +3,56 @@ require 'spec_helper'
 describe 'tuskar::db::postgresql' do
 
   let :req_params do
-    {:password => 'pw'}
+    { :password => 'pw' }
   end
 
-  let :facts do
-    {
-      :postgres_default_version => '8.4',
-      :osfamily => 'RedHat',
-    }
+  let :pre_condition do
+    'include postgresql::server'
   end
 
-  describe 'with only required params' do
-    let :params do
-      req_params
+  context 'on a RedHat osfamily' do
+    let :facts do
+      {
+        :osfamily                 => 'RedHat',
+        :operatingsystemrelease   => '7.0',
+        :concat_basedir => '/var/lib/puppet/concat'
+      }
     end
-    it { is_expected.to contain_postgresql__db('tuskar').with(
-      :user         => 'tuskar',
-      :password     => 'pw'
-     ) }
+
+    context 'with only required parameters' do
+      let :params do
+        req_params
+      end
+
+      it { should contain_postgresql__server__db('tuskar').with(
+        :user     => 'tuskar',
+        :password => 'md532cc9d9d42efb508bc9d6267d873d63d'
+      )}
+    end
+
+  end
+
+  context 'on a Debian osfamily' do
+    let :facts do
+      {
+        :operatingsystemrelease => '7.8',
+        :operatingsystem        => 'Debian',
+        :osfamily               => 'Debian',
+        :concat_basedir => '/var/lib/puppet/concat'
+      }
+    end
+
+    context 'with only required parameters' do
+      let :params do
+        req_params
+      end
+
+      it { should contain_postgresql__server__db('tuskar').with(
+        :user     => 'tuskar',
+        :password => 'md532cc9d9d42efb508bc9d6267d873d63d'
+      )}
+    end
+
   end
 
 end
