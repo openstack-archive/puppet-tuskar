254cc05262d957b8813a5ecff4137a537ccdea94
Emilien Macchi <emilien@redhat.com>
2015-03-13 09:30:25 -0400

    Add UI support (+ UI extras)
    Implement tuskar::ui with:
    * tuskar UI package
    * optionaly and disabled by default, tuskar UI extras package
    * allow to not manage the package
    
    This patch also brings consistency with other modules/classes in renaming ensure_package to package_ensure.
    
    Change-Id: I8dd03dffbb2c36c7cdfc82681444cecb6e87021c

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,11 +7,15 @@ class tuskar::params {
       $client_package_name      = 'openstack-tuskar'
       $api_package_name         = 'openstack-tuskar-api'
       $api_service_name         = 'openstack-tuskar-api'
+      $ui_package_name          = 'openstack-tuskar-ui'
+      $ui_extras_package_name   = 'openstack-tuskar-ui-extras'
     }
     'Debian': {
       $client_package_name      = 'python-tuskarclient'
       $api_package_name         = 'tuskar-api'
       $api_service_name         = 'tuskar-api'
+      $ui_package_name          = 'tuskar-ui'
+      $ui_extras_package_name   = 'tuskar-ui-extras'
     }
     default: {
       fail("Unsupported osfamily: ${::osfamily} operatingsystem")
