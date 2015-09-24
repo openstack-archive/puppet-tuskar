2a8cc7268405dfddd8b73d875602c6a84a2c7b87
Yanis Guenane <yguenane@redhat.com>
2015-09-24 12:08:22 +0200

    Introduce tuskar::db class
    Current modules[1][2][3] implements a
    <component>::db class that is not implemented in tuskar.
    
    This commit aims to apply here the same logic.
    
    [1] https://github.com/openstack/puppet-nova/blob/master/manifests/db.pp
    [2]
    https://github.com/openstack/puppet-designate/blob/master/manifests/db.pp
    [3]
    https://github.com/openstack/puppet-ceilometer/blob/master/manifests/db.pp
    
    Change-Id: Ifa44d3f4afb912b912526141111d8dda53caed0f

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,6 +9,8 @@ class tuskar::params {
       $api_service_name         = 'openstack-tuskar-api'
       $ui_package_name          = 'openstack-tuskar-ui'
       $ui_extras_package_name   = 'openstack-tuskar-ui-extras'
+      $psycopg_package_name     = 'python-psycopg2'
+      $sqlite_package_name      = undef
     }
     'Debian': {
       $client_package_name      = 'python-tuskarclient'
@@ -16,6 +18,8 @@ class tuskar::params {
       $api_service_name         = 'tuskar-api'
       $ui_package_name          = 'tuskar-ui'
       $ui_extras_package_name   = 'tuskar-ui-extras'
+      $psycopg_package_name     = 'python-psycopg2'
+      $sqlite_package_name      = 'python-pysqlite2'
     }
     default: {
       fail("Unsupported osfamily: ${::osfamily} operatingsystem")
