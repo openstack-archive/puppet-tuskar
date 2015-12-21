cb68e4142ed17ab220cde667cd409eb8fa49b4c0
Emilien Macchi <emilien@redhat.com>
2015-12-21 20:52:32 +0100

    Make Keystone_endpoint match service by name/type
    Since a chance in puppet-keystone (1], we now match an endpoint with a
    service name/type.
    )
    [1] http://git.openstack.org/cgit/openstack/puppet-keystone/commit/?id=0a4e06abb0f5b3f324464ff5219d2885816311ce
    
    Change-Id: I09c80c1c9b816a4dc734a268010fe8569ca8ec03
    Closes-Bug: #1528308

--- spec/classes/tuskar_keystone_auth_spec.rb
+++ spec/classes/tuskar_keystone_auth_spec.rb
@@ -25,13 +25,12 @@ describe 'tuskar::keystone::auth' do
       :roles   => ['admin']
     )}
 
-    it { is_expected.to contain_keystone_service('tuskar').with(
+    it { is_expected.to contain_keystone_service('tuskar::management').with(
       :ensure      => 'present',
-      :type        => 'management',
       :description => 'Tuskar Management Service'
     ) }
 
-    it { is_expected.to contain_keystone_endpoint('RegionOne/tuskar').with(
+    it { is_expected.to contain_keystone_endpoint('RegionOne/tuskar::management').with(
       :ensure       => 'present',
       :public_url   => "http://127.0.0.1:8585",
       :admin_url    => "http://127.0.0.1:8585",
@@ -47,7 +46,7 @@ describe 'tuskar::keystone::auth' do
         :internal_url => 'https://10.0.0.11:8585' }
     end
 
-    it { is_expected.to contain_keystone_endpoint('RegionOne/tuskar').with(
+    it { is_expected.to contain_keystone_endpoint('RegionOne/tuskar::management').with(
       :ensure       => 'present',
       :public_url   => 'https://10.0.0.10:8585',
       :admin_url    => 'https://10.0.0.11:8585',
@@ -77,7 +76,7 @@ describe 'tuskar::keystone::auth' do
         :admin_address    => '10.10.10.12' }
     end
 
-    it { is_expected.to contain_keystone_endpoint('RegionOne/tuskar').with(
+    it { is_expected.to contain_keystone_endpoint('RegionOne/tuskar::management').with(
       :ensure       => 'present',
       :public_url   => "https://10.10.10.10:80",
       :internal_url => "http://10.10.10.11:81",
@@ -93,8 +92,8 @@ describe 'tuskar::keystone::auth' do
 
     it { is_expected.to contain_keystone_user('tuskary') }
     it { is_expected.to contain_keystone_user_role('tuskary@services') }
-    it { is_expected.to contain_keystone_service('tuskary') }
-    it { is_expected.to contain_keystone_endpoint('RegionOne/tuskary') }
+    it { is_expected.to contain_keystone_service('tuskary::management') }
+    it { is_expected.to contain_keystone_endpoint('RegionOne/tuskary::management') }
   end
 
   describe 'when overriding various parameters' do
